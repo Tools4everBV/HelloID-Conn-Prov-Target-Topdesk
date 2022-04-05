f1ff82a0f6f278955403b10fbf86fff2b2ecb5cb
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2022-04-05 15:51:44 +0200

    new version, might be improved later

---
+++ permission_change_retrieve.ps1
@@ -0,0 +1,24 @@
+#####################################################
+# HelloID-Conn-Prov-Target-TOPdesk-Permissions-Changes
+#
+# Version: 2.0
+#####################################################
+
+# Initialize default values
+$config = $configuration | ConvertFrom-Json
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Enable debug verbose logging
+$VerbosePreference = 'Continue'
+
+try {
+    $permissionList = Get-Content -Raw -Path $config.notificationJsonPath | ConvertFrom-Json
+    $permissions = $permissionList | Select-Object -Property displayName, identification
+    write-output $permissions | ConvertTo-Json -Depth 10
+} catch {
+    $ex = $PSItem
+    $errorMessage = "Could not retrieve TOPdesk permissions. Error: $($ex.Exception.Message)"
+    Write-Verbose $errorMessage
+}
