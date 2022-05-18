7dbac28c51cfc7b0f963d9a7a0b3f90e9d1ce078
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2022-05-18 10:38:27 +0200

    new permission grant script

--- permission_change_grant.ps1
+++ permission_change_grant.ps1
@@ -79,10 +79,11 @@ function Invoke-TopdeskRestMethod {
                 Method      = $Method
                 ContentType = $ContentType
             }
+
             if ($Body) {
-                Write-Verbose 'Adding body to request'
                 $splatParams['Body'] = [Text.Encoding]::UTF8.GetBytes($Body)
             }
+
             Invoke-RestMethod @splatParams -Verbose:$false
         } catch {
             $PSCmdlet.ThrowTerminatingError($_)
@@ -114,7 +115,7 @@ function Get-HelloIdTopdeskTemplateById {
 
     # Check if file exists.
     try {
-        $permissionList = Get-Content -=JsonPath $config.notificationJsonPath
+        $permissionList = Get-Content -Raw -Path $config.notificationJsonPath | ConvertFrom-Json
     } catch {
         $ex = $PSItem
         $errorMessage = "Could not retrieve Topdesk permissions file. Error: $($ex.Exception.Message)"
@@ -137,7 +138,7 @@ function Get-HelloIdTopdeskTemplateById {
     }
 
     # Check if entitlement with id and specific type exists
-    if (-not($entitlementSet.Keys -Contains $type)) {
+    if (-not($entitlementSet.PSObject.Properties.Name -Contains $type)) {
         $errorMessage = "Could not find grant entitlement for entitlementSet '$($pRef.id)'. This is likely an issue with the json file."
         $auditLogs.Add([PSCustomObject]@{
             Message = $errorMessage
@@ -188,10 +189,10 @@ function Get-TopdeskTemplateById {
     }
     $responseGet = Invoke-TopdeskRestMethod @splatParams
 
-    $topdeskTemplate = $responseGet.results | Where-Object { ($_.number -eq $TemplateId) }
+    $topdeskTemplate = $responseGet.results | Where-Object { ($_.number -eq $Id) }
 
     if ([string]::IsNullOrEmpty($topdeskTemplate)) {
-        $errorMessage = "Topdesk template [$TemplateId] not found. Please verify this template exists and it's available for the API in Topdesk."
+        $errorMessage = "Topdesk template [$Id] not found. Please verify this template exists and it's available for the API in Topdesk."
         $auditLogs.Add([PSCustomObject]@{
             Message = $errorMessage
             IsError = $true
@@ -199,7 +200,7 @@ function Get-TopdeskTemplateById {
         return
     }
 
-    Write-Output $topdeskTemplate
+    Write-Output $topdeskTemplate.id
 }
 
 function Format-Description {
@@ -211,13 +212,22 @@ function Format-Description {
         $Description
     )
 
-    $descriptionFormatted = Invoke-Expression "`"$Description`""
+    $descriptionFormatted = $ExecutionContext.InvokeCommand.ExpandString($Description)
     Write-Output $descriptionFormatted
 }
 
 function Get-TopdeskRequesterByType {
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $BaseUrl,
+
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]
+        $Headers,
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [string]
@@ -228,6 +238,9 @@ function Get-TopdeskRequesterByType {
 
         [string]
         $managerAccountReference,     # optional, only required when type is manager
+
+        [string]
+        $managerFallback,             # optional, will be used when the manager reference is empty
 
         [System.Collections.Generic.List[PSCustomObject]]
         [ref]$AuditLogs
@@ -249,43 +262,61 @@ function Get-TopdeskRequesterByType {
 
     # Validate employee entry
     if ($type -eq 'manager') {
-        if ([string]::IsNullOrEmpty($managerAccountReference)) {
-            $errorMessage = "Could not set requester: The manager account reference is empty."
+        if ([string]::IsNullOrEmpty($managerAccountReference) -And [string]::IsNullOrEmpty($managerFallback)) {
+            
+            $errorMessage = "Could not set requester: The manager account reference is empty and no fallback email is configured."
             $auditLogs.Add([PSCustomObject]@{
                 Message = $errorMessage
                 IsError = $true
             })
+            return
         } else {
-            Write-Output $managerAccountReference
+            if (-Not [string]::IsNullOrEmpty($managerAccountReference)) {
+                Write-Output $managerAccountReference
+                return
+            }
         }
-        return
+        # Set fallback adress and look it up below
+        $type = $managerFallback
     }
 
-    # Query email address
+    # Query email address (manager fallback or static)
     $splatParams = @{
-        Uri     = "$baseUrl/tas/api//persons?email=$type"
+        Uri     = "$baseUrl/tas/api/persons?email=$type"
         Method  = 'GET'
         Headers = $Headers
     }
     $responseGet = Invoke-TopdeskRestMethod @splatParams        #todo: have to find out what the response looks like
 
-    #Validate static requester
-    if ([string]::IsNullOrEmpty($responseGet)) {
-        $errorMessage = "Could not set requester: Topdesk person with email [$Type] not found."
+    # Check if only one result is returned
+    if ([string]::IsNullOrEmpty($responseGet.id)) {
+
+        # no results found
+         $errorMessage = "Could not set requester: Topdesk person with email [$Type] not found."
         $auditLogs.Add([PSCustomObject]@{
             Message = $errorMessage
             IsError = $true
         })
         return
+    } elseif ($responseGet.Count -eq 1) {
+
+        # one record found, correlate, return id
+        write-output $responseGet.id
+    } else {
+
+        # Multiple records found, correlation
+        $errorMessage = "Multiple [$($responseGet.Count)] persons found with Email address [$Email]. Login names: [$($responseGet.tasLoginName)]"
+        $auditLogs.Add([PSCustomObject]@{
+            Message = $errorMessage
+            IsError = $true
+        })
     }
-    Write-Output $responseGet.id
 }
 
 function Get-TopdeskChangeType {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        #[ValidateNotNullOrEmpty()]
         [string]
         $changeType,                        # 'simple' or 'extensive'
 
@@ -316,6 +347,41 @@ function Get-TopdeskChangeType {
     return $ChangeType.ToLower()
 }
 
+
+function New-TopdeskChange {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $BaseUrl,
+
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]
+        $Headers,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [PsObject]
+        $TopdeskChange,
+
+        [System.Collections.Generic.List[PSCustomObject]]
+        [ref]$AuditLogs
+    )
+
+    $splatParams = @{
+        Uri     = "$baseUrl/tas/api/operatorChanges"
+        Method  = 'POST'
+        Headers = $Headers
+        Body    = $TopdeskChange | ConvertTo-Json
+    }
+    $change = Invoke-TopdeskRestMethod @splatParams
+
+    Write-Verbose "Created change with number [$($change.number)]"
+
+    Write-Output $change
+}
+
 #endregion
 
 try {
@@ -328,16 +394,17 @@ try {
     #Get-TopdeskChangePriority
 
 #region lookuptemplate
-    if ($config.notifications.disable -eq $true) {
+    if ($config.disableNotifications -eq $true) {
         Throw "Notifications are disabled"
     }
-    
-# Lookup template from json file
+    $action = 'Process'
+
+    # Lookup template from json file (C00X)
     $splatParamsHelloIdTopdeskTemplate = @{
-        Headers          = $Headers
-        baseUrl          = $baseUrl
-        Id               = $pRef.id
-        AuditLogs        = [Ref]$auditLogs
+        JsonPath        = $config.notificationJsonPath
+        Id              = $pRef.id
+        Type            = "Grant"
+        AuditLogs       = [Ref]$auditLogs
     }
     $template = Get-HelloIdTopdeskTemplateById @splatParamsHelloIdTopdeskTemplate
 
@@ -346,10 +413,13 @@ try {
         Throw 'HelloID template not found'
     }
 
+    # Setup authentication headers
+    $authHeaders = Set-AuthorizationHeaders -UserName $Config.username -ApiKey $Config.apiKey
+
     # Lookup Topdesk template id (sja xyz)
     $splatParamsTopdeskTemplate = @{
-        Headers          = $Headers
-        baseUrl          = $baseUrl
+        Headers          = $authHeaders
+        baseUrl          = $config.baseUrl
         Id               = $template.Template
         AuditLogs        = [Ref]$auditLogs
     }
@@ -387,12 +457,13 @@ try {
 
     # Resolve requester
     $splatParamsTopdeskRequester = @{
-        Headers                 = $Headers
-        baseUrl                 = $baseUrl
+        Headers                 = $authHeaders
+        baseUrl                 = $config.baseUrl
         Type                    = $template.Requester
         accountReference        = $aRef
         managerAccountReference = $mRef
-        AuditLogs               = $auditLogs
+        managerFallback         = $config.notificationRequesterFallback
+        AuditLogs               = [Ref]$auditLogs
     }
     $requesterId = Get-TopdeskRequesterByType @splatParamsTopdeskRequester
 
@@ -409,12 +480,10 @@ try {
         AuditLogs        = [Ref]$auditLogs
     }
     $changeType = Get-TopdeskChangeType @splatParamsTopdeskTemplate
-    
+
     # Add value to request object
     $requestObject += @{
-        changeType  = @{
-            changeType = $changeType
-        }
+        changeType = $changeType
     }
 
     ## Support for optional parameters, are only added when they exist and are not set to null
@@ -467,70 +536,110 @@ try {
         }
     }
 
-    # alles is goed. 
-
-
-    # Something with the person. If the person is archived, unarchive the person (employee/manager) rearchive afterwards
-
-
-
-
-
-
-
-
-
-
+    if ($auditLogs.isError -contains -$true) {
+        Throw "Error(s) occured while looking up required values"
+    }
 
     # Add an auditMessage showing what will happen during enforcement
     if ($dryRun -eq $true) {
+
         $auditLogs.Add([PSCustomObject]@{
-            Message = "Grant Topdesk entitlement: [$($pRef.Reference)] to: [$($p.DisplayName)], will be executed during enforcement"
+            Message = "Grant Topdesk entitlement: [$($pRef.id)] to: [$($p.DisplayName)], will be executed during enforcement"
         })
+        Write-Verbose ($requestObject | ConvertTo-Json)
     }
 
     if (-not($dryRun -eq $true)) {
-        Write-Verbose "Granting TOPdesk entitlement: [$($pRef.Reference)] to: [$($p.DisplayName)]"
+        Write-Verbose "Granting TOPdesk entitlement: [$($pRef.id)] to: [$($p.DisplayName)]"
+
+        # Create change in Topdesk
+        $splatParamsTopdeskChange = @{
+            Headers                 = $authHeaders
+            baseUrl                 = $config.baseUrl
+            TopdeskChange           = $requestObject
+            AuditLogs               = [Ref]$auditLogs
+        }
+        $TopdeskChange = New-TopdeskChange @splatParamsTopdeskChange
+    
+    # Todo: stuff to requester (better make this a function)
+    # Something with the person. If the person is archived, unarchive the person (employee/manager) rearchive afterwards
+    # # Prepare manager record, if manager has to be set
+    # if (-Not([string]::IsNullOrEmpty($account.manager.id))) {
+    #     if ($TopdeskManager.isManager -eq $false) {
+    #         if ($TopdeskManager.status -eq 'personArchived') {
+
+    #             # Unarchive person
+    #             $shouldArchive  = $true
+    #             $splatParamsManagerUnarchive = @{
+    #                 TopdeskPerson   = [ref]$TopdeskManager
+    #                 Headers         = $authHeaders
+    #                 BaseUrl         = $config.baseUrl
+    #                 Archive         = $false
+    #             }
+    #             Set-TopdeskPersonArchiveStatus @splatParamsManagerUnarchive
+    #         }
+
+    #         # Set isManager to true
+    #         $splatParamsManagerIsManager = @{
+    #             TopdeskPerson   = [ref]$TopdeskManager
+    #             Headers         = $authHeaders
+    #             BaseUrl         = $config.baseUrl
+    #             IsManager       = $true
+    #         }
+    #         Set-TopdeskPersonIsManager @splatParamsManagerIsManager
+
+    #         # Archive manager if required
+    #         if ($shouldArchive -and $TopdeskManager.status -ne 'personArchived') {
+
+    #             # Archive manager
+    #             $splatParamsManagerArchive = @{
+    #                 TopdeskPerson   = [ref]$TopdeskManager
+    #                 Headers         = $authHeaders
+    #                 BaseUrl         = $config.baseUrl
+    #                 Archive         = $true
+    #             }
+    #             Set-TopdeskPersonArchiveStatus @splatParamsManagerArchive
+    #         }
+    #     }
+    # }
 
         $success = $true
         $auditLogs.Add([PSCustomObject]@{
-            Message = "Grant TOPdesk entitlement: [$($pRef.Reference)] to: [$($p.DisplayName)] was successful."
+            Message = "Grant TOPdesk entitlement: [$($pRef.id)] with number [$($change.number)] was successful."
             IsError = $false
         })
     }
 } catch {
     $success = $false
     $ex = $PSItem
-    #another option: "Notifications are disabled"
-
-    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
-        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
-        $errorMessage = "Could not $action person. Error: $($ex.ErrorDetails.Message)"
-    } else {
-        $errorMessage = "Could not $action person. Error: $($ex.Exception.Message) $($ex.ScriptStackTrace)"
-    } 
-
+    Write-Verbose ($ex | ConvertTo-Json)
     switch ($ex.Exception.Message) {
 
         'HelloID Template not found' { 
-                # Only log when there are no lookup values, as these generate their own audit message
-                $success = -not($auditLogs.isError -contains $true)
-        } 
-        
+                # Only log when there are no lookup values, as these generate their own audit message, set success based on error state
+                $success = -Not($auditLogs.isError -contains $true)
+        }
+
         'Error(s) occured while looking up required values' { 
             # Only log when there are no lookup values, as these generate their own audit message
         }
 
         'Notifications are disabled' {
-            # Don't do anything when notifications are disabled
+            # Don't do anything when notifications are disabled, mark them as a success
+            $success = true
             $message = 'Not creating Topdesk change, because the notifications are disabled in the connector configuration.'
             $auditLogs.Add([PSCustomObject]@{
                 Message = $message
                 IsError = $false
             })
-            
 
         } default {
+            if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+                $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+                $errorMessage ="Could not grant TOPdesk entitlement: [$($pRef.id)]. Error: $($ex.ErrorDetails.Message)"
+            } else {
+                $errorMessage = "Could not grant TOPdesk entitlement: [$($pRef.id)]. Error: $($ex.Exception.Message) $($ex.ScriptStackTrace)"
+            } 
             $auditLogs.Add([PSCustomObject]@{
                 Message = $errorMessage
                 IsError = $true
