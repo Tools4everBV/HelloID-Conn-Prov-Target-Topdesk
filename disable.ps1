91cce0a3a4437f14ffa45f29ac945d3120bd3c5d
bvandervoorn <87750268+bvandervoorn@users.noreply.github.com>
2022-12-06 16:44:19 +0100

    Update disable.ps1
    Write-verbose -message causes issues in the cloud agent. Removed -message.

--- disable.ps1
+++ disable.ps1
@@ -1,4 +1,4 @@
-﻿#Initialize default properties
+#Initialize default properties
 $success = $false
 $p = $person | ConvertFrom-Json
 $aRef = $accountReference | ConvertFrom-Json
@@ -33,11 +33,11 @@ if(-Not($dryRun -eq $true)){
     try {
         $lookupFailure = $false
 
-        Write-Verbose -Message "Archiving reason lookup..."
+        Write-Verbose -Verbose "Archiving reason lookup..."
         if ([string]::IsNullOrEmpty($PersonArchivingReason.id)) {
             $auditMessage = $auditMessage + "; Archiving reason is not set'"
             $lookupFailure = $true
-            Write-Verbose -Verbose -Message "Archiving reason lookup failed"
+            Write-Verbose -Verbose "Archiving reason lookup failed"
         } else {
             $archivingReasonUrl = $url + "/archiving-reasons"
             $responseArchivingReasonJson = Invoke-WebRequest -uri $archivingReasonUrl -Method Get -Headers $headers -UseBasicParsing
@@ -45,17 +45,17 @@ if(-Not($dryRun -eq $true)){
             $archivingReason = $responseArchivingReason | Where-object name -eq $PersonArchivingReason.id
 
             if ([string]::IsNullOrEmpty($archivingReason.id) -eq $true) {
-                Write-Verbose -Verbose -Message "Archiving Reason '$($PersonArchivingReason.id)' not found"
+                Write-Verbose -Verbose "Archiving Reason '$($PersonArchivingReason.id)' not found"
                 $auditMessage = $auditMessage + "; Archiving Reason '$($PersonArchivingReason.id)' not found"
                 $lookupFailure = $true
-                Write-Verbose -Verbose -Message "Archiving Reason lookup failed"
+                Write-Verbose -Verbose "Archiving Reason lookup failed"
             } else {
                 $PersonArchivingReason.id = $archivingReason.id
-                Write-Verbose -Verbose -Message "Archiving Reason lookup succesful"
+                Write-Verbose -Verbose "Archiving Reason lookup succesful"
             }
         }
 
-        Write-Verbose -Verbose -Message "Person lookup..."
+        Write-Verbose -Verbose "Person lookup..."
         $PersonUrl = $url + "/persons/id/${aRef}"
         $responsePersonJson = Invoke-WebRequest -uri $PersonUrl -Method Get -Headers $headers -UseBasicParsing
         $responsePerson = $responsePersonJson.Content | Out-String | ConvertFrom-Json
@@ -63,26 +63,26 @@ if(-Not($dryRun -eq $true)){
         if([string]::IsNullOrEmpty($responsePerson.id)) {
             $auditMessage = $auditMessage + "; Person is not found in TOPdesk'"
             $lookupFailure = $true
-            Write-Verbose -Verbose -Message "Person not found in TOPdesk"
+            Write-Verbose -Verbose "Person not found in TOPdesk"
         } else {
-            Write-Verbose -Verbose -Message "Person lookup succesful"
+            Write-Verbose -Verbose "Person lookup succesful"
         }
 
         if (!($lookupFailure)) {
-			Write-Verbose -Verbose -Message "Updating account for '$($p.ExternalID)...'"
+			Write-Verbose -Verbose "Updating account for '$($p.ExternalID)...'"
 			$bodyPersonUpdate = $account | ConvertTo-Json -Depth 10
 			$null = Invoke-WebRequest -uri $personUrl -Method PATCH -Headers $headers -Body  ([Text.Encoding]::UTF8.GetBytes($bodyPersonUpdate)) -UseBasicParsing
-			Write-Verbose -Verbose -Message "Updated account for '$($p.ExternalID)...'"
+			Write-Verbose -Verbose "Updated account for '$($p.ExternalID)...'"
 
             if ($responsePerson.status -eq "person") {
-                Write-Verbose -Verbose -Message "Archiving account for '$($p.ExternalID)...'"
+                Write-Verbose -Verbose "Archiving account for '$($p.ExternalID)...'"
                 $bodyPersonArchive = $PersonArchivingReason | ConvertTo-Json -Depth 10
                 $archiveUrl = $url + "/persons/id/${aRef}/archive"
                 $null = Invoke-WebRequest -uri $archiveUrl -Method PATCH -Body ([Text.Encoding]::UTF8.GetBytes($bodyPersonArchive)) -Headers $headers -UseBasicParsing
-                Write-Verbose -Verbose -Message "Account Archived"
+                Write-Verbose -Verbose "Account Archived"
                 $auditMessage = "disabled succesfully"
             } else {
-                Write-Verbose -Verbose -Message "Person is already archived. Nothing to do"
+                Write-Verbose -Verbose "Person is already archived. Nothing to do"
             }
             $success = $true
             $auditMessage = "disabled succesfully"
@@ -90,13 +90,13 @@ if(-Not($dryRun -eq $true)){
 
     } catch {
         if ($_.Exception.Response.StatusCode -eq "Forbidden") {
-            Write-Verbose -Verbose -Message "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
+            Write-Verbose -Verbose "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.Exception.Message)'"
             $auditMessage = " not disabled succesfully: '$($_.Exception.Message)'"
         } elseif (![string]::IsNullOrEmpty($_.ErrorDetails.Message)) {
-            Write-Verbose -Verbose -Message "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
+            Write-Verbose -Verbose "Something went wrong $($_.ScriptStackTrace). Error message: '$($_.ErrorDetails.Message)'"
             $auditMessage = " not disabled succesfully: '$($_.ErrorDetails.Message)'"
         } else {
-            Write-Verbose -Verbose -Message "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
+            Write-Verbose -Verbose "Something went wrong $($_.ScriptStackTrace). Error message: '$($_)'"
             $auditMessage = " not disabled succesfully: '$($_)'"
         }
         $success = $false
