3b25a4147acb515f2733491812dfe0e5b0890f77
rhouthuijzen <116062840+rhouthuijzen@users.noreply.github.com>
2025-10-20 12:14:59 +0200

    Fix: make sure there is no merge conflict
    PR 41 and 42 where active at the same time

--- import.ps1
+++ import.ps1
@@ -48,7 +48,7 @@ try {
 
     $existingAccounts = @()
     $pageSize = 5000
-    $uri = "$($actionContext.Configuration.baseUrl)/tas/api/persons?pageStart=0&pageSize=$pageSize&fields=$fields"
+    $uri = "$($actionContext.Configuration.baseUrl)/tas/api/persons?pageStart=0&pageSize=$pageSize&fields=$fields&query=employeeNumber != '*****'"
     
     do {
         $splatParams = @{
